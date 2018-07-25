faf0d00085c2097ec8ab8c0064257ce9c7aa0511
yunusemretatar <yunusemre.tatar@sekomyazilim.com.tr>
2018-07-25 17:46:27 +0300

    externals node_modules

--- webpack.release.js
+++ webpack.release.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = (env) => ({
 	mode: 'production',
@@ -8,5 +9,15 @@ module.exports = (env) => ({
 	output: {
 		path: path.resolve(__dirname, './' ),
 		filename: "index.js"
-	}
+	},
+	module: {
+		rules: [
+		  {
+			exclude: "/node_modules/"
+		  }
+		]
+	},
+	target: 'node',
+    externals: [nodeExternals()],
+	plugins: [new BundleAnalyzerPlugin()]
 });
